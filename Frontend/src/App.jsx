76f592f0aa95e77cb3cbfadc8786d8329dbb25ef
Erick De Belen <erickdebelen22@gmail.com>
2025-06-29 10:21:17 +0800

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './styles/App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
--- Frontend/src/config/api.js
+++ Frontend/src/config/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'https://gerardify-vercel-adykar6lx-erick-de-belens-projects.vercel.app/api';
+const API_BASE_URL = import.meta.env.PROD 
+  ? 'https://gerardify-vercel-adykar6lx-erick-de-belens-projects.vercel.app/api'
+  : 'http://localhost:5000/api';
 
 export default API_BASE_URL;
